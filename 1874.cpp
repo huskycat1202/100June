ed5972912b26bbc2579708a1f038e18b8bf3a48f
huskycat1202 <50573849+huskycat1202@users.noreply.github.com>
2020-01-09 22:21:10 +0900

    Add files via upload

---
+++ 1874.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+	int n;
+	cin >> n; // 8
+	int memo[100000]={};
+	for(int i=0; i<n; i++){
+		cin >> memo[i]; // 4 3 6 8 7 5 2 1
+	}
+	while(1){
+		
+	}
+	
+}
