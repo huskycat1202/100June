702afdec1b878c65ed6ca01734637e2257b73999
huskycat1202 <50573849+huskycat1202@users.noreply.github.com>
2020-01-28 14:28:10 +0900

    Add files via upload

---
+++ 10825.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	string x;
+	vector< pair< int, pair< int, pair< int, string> > > > v;
+	for(int i=0; i<n; i++){
+		int k, e, m;
+		cin >> x >> k >> e >> m;
+		v.push_back({-k, {e, {-m, x}}});
+	}
+	sort(v.begin(), v.end());
+	for(int i=0; i<n; i++){
+		cout << v[i].second.second.second << "\n";
+	}
+}
