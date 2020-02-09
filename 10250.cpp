6dbe34bfaa8d7db5d2a5df38b5b33ef4ad9b1cb7
huskycat1202 <50573849+huskycat1202@users.noreply.github.com>
2020-02-09 17:35:08 +0900

    Add files via upload

---
+++ 10250.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int t,h,w,n; // Ãþ È£ ¸î¹øÂ° 
+	int a,b; 
+	cin >> t;
+	while(t--){
+		cin >> h >> w >> n;
+		if(n%h==0){
+			if(n/h<10) cout << h << "0" << n/h;
+			else cout << h << n/h;
+		}
+		else{
+			a=(n/h)+1; // È£  
+			b=n%h; // Ãþ 
+			if(a<10) cout << b << "0" << a;
+			else cout << b << a;
+		}
+	}
+}
