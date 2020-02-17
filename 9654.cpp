d3f7ea06eff309e2b62fa0e7c92ff41ce271e0e8
huskycat1202 <50573849+huskycat1202@users.noreply.github.com>
2020-02-18 01:45:10 +0900

    Add files via upload

---
+++ 9654.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout << "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE";
+	cout << "\n";
+	cout << "N2 Bomber      Heavy Fighter  Limited    21        ";
+	cout << "\n";
+	cout << "J-Type 327     Light Combat   Unlimited  1         ";
+	cout << "\n";
+	cout << "NX Cruiser     Medium Fighter Limited    18        ";
+	cout << "\n";
+	cout << "N1 Starfighter Medium Fighter Unlimited  25        ";
+	cout << "\n";
+	cout << "Royal Cruiser  Light Combat   Limited    4         ";
+}
