4e82a743e40bb53cd436440ef64e966c93894819
huskycat1202 <annie991202@naver.com>
2020-03-03 01:55:22 +0900

    20.03.03

--- 10816.cpp
+++ 10816.cpp
@@ -1,26 +1,16 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
+int cnt[20000002]={},n,m,x;
 int main(){
-    int n,m,N[500001]={},M[500001]={},ans[500001]={};
     cin >>n;
     for(int i=0; i<n; i++){
-        cin >> N[i];
+        cin >> x;
+        cnt[x+10000000]++;
     }
-    sort(N,N+n);
     cin >> m;
     for(int i=0; i<m; i++){
-        cin >> M[i];
-    }
-    sort(M,M+m);
-
-    for(int i=0; i<m; i++){
-        if(N[n/2]==M[i]) ans[i]++;
-        else if(N[n/2]<M[i])
-        for(int j=0; j<n; j++){
-            if(N[j]==M[i]) ans[i]++;
-        }
-    }
-    for(int i=0; i<m; i++){
-        cout << ans[i] << " ";
+        cin >> x;
+        cout<< cnt[x+10000000] << " ";
     }
 }
