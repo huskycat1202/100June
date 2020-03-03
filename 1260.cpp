e50d8f2d3f1057f8fbc67e39f0642f1c23356cc6
huskycat1202 <annie991202@naver.com>
2020-03-04 02:04:32 +0900

    20.03.04

--- 1260.cpp
+++ 1260.cpp
@@ -1,17 +1,61 @@
 #include<iostream>
+#include<queue>
+#include<vector>
+#include<algorithm>
 using namespace std;
-int main(){
-    int n,m,v,graph[10000][2]; cin >> n >> m >> v;
-    for(int i=0; i<m; i++){
-        cin >> graph[i][0] >> graph[i][1];
+int n,m,v,x,y;
+vector <int> gr[1001];
+//dfs
+bool dvisited[1001]={};
+void dfs(int v){
+    cout << v << ' ';
+    for(int i:gr[v]){
+        if(!dvisited[i]){
+            dvisited[i]=1;
+            dfs(i);
+        }
     }
-    
+}
+
+//bfs
+bool bvisited[1001]={};
+void bfs(){
+    queue <int> q;
+    q.push(v);
+    bvisited[v]=1;
+    while(!q.empty()){
+        int f = q.front();
+        cout << f << ' ';
+        q.pop();
+        for(int i:gr[f]){
+            if(!bvisited[i]){
+                bvisited[i]=1;
+                q.push(i);
+            }
+        }
+    }
+}
+
+int main(){
+    cin >> n >> m >> v;
+    for(int i=0; i<m; i++){
+        cin >> x >> y;
+        gr[x].push_back(y);
+        gr[y].push_back(x);
+    }
+    for(int i=1; i<=n; i++){
+        sort(gr[i].begin(),gr[i].end());
+    }
+    dvisited[v]=1;
+    dfs(v);
+    cout<<'\n';
+    bfs();
 }
 
 /*
-5 4
-5 2
-1 2
-3 4
-3 1
+gr[1] 2 3
+gr[2] 1 5
+gr[3] 1 4
+gr[4] 3 5
+gr[5] 2 4
 */
