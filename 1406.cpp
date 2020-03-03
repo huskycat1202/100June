dbf9e11a48f57ee5bfa8d8cb5d86da5f852d22d5
huskycat1202 <annie991202@naver.com>
2020-03-03 17:48:58 +0900

    20.03.03

--- 1406.cpp
+++ 1406.cpp
@@ -1,25 +1,38 @@
 #include<iostream>
+#include<stack>
 #include<string>
 using namespace std;
+stack <char> l;
+stack <char> r;
+string L,R;
+char k,s;
+int m;
 int main(){
-    char line[600001],x;
-    int cur=0,m,i=0;
-    while(line[i]!='\0'){
-        cin >> line[i];
-        i++;
-        cur++;
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> L;
+    for(int i=0; L[i]!='\0'; i++){
+        l.push(L[i]);
     }
+    L="";
     cin >> m;
-    for(int i=0; i<m; i++){
-        cin >> x;
-        if(x=='L' && cur>0) cur--;
-        else if(x=='D' && cur<i) cur++;
-        else if(x=='B' && cur>0){
-            cur--;
-
-        }
-        else{
-            cin >> line[i]
-        }
+    while(m--){
+        cin >> k;
+        if(k=='P'){cin >> s; l.push(s);}
+        else if(k=='L' && !l.empty()){s=l.top(); l.pop(); r.push(s);}
+        else if(k=='D' && !r.empty()){s=r.top(); r.pop(); l.push(s);}
+        else if(k=='B' && !l.empty()){l.pop();}
+    }
+    while(!l.empty()){
+        s=l.top();
+        L+=s;
+        l.pop();
+    }
+    for(int i=L.size()-1; i>=0; i--){
+        cout << L[i];
+    }
+    while(!r.empty()){
+        cout << r.top();
+        r.pop();
     }
 }
