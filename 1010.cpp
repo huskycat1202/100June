2a36b44f2a088c6a9b999b6030b6fe810b0aaa7a
huskycat1202 <annie991202@naver.com>
2020-03-11 16:43:08 +0900

    20.03.11

--- 1010.cpp
+++ 1010.cpp
@@ -1,22 +1,20 @@
 #include<iostream>
 using namespace std;
-long long int t,n,m,sum=1;
-int fac(int x){
-    if(x==0) return 1;
-    for(int i=1; i<=x; i++){
-        sum*=i;
-    }
-    return sum;
-    sum=0;
-}
-int ans(int a, int b){
-    for(int i=a; i>=1)
-    return fac(a)/fac(a-b+1);
-}
+long long int t,n,m,bri[30][30]={};
 int main(){
     cin >> t;
+    for(int i=1; i<30; i++){
+        bri[1][i]=i;
+        bri[i][i]=1;
+        
+    }
+    for(int i=2; i<30; i++){
+        for(int j=3; j<30; j++){
+            bri[i][j]=bri[i][j-1]+bri[i-1][j-1];
+        }
+    }
     while(t--){
         cin >> n >> m;
-        cout << C(m,n) << "\n";
+        cout << bri[n][m] << "\n";
     }
 }
