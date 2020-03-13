7eed346757de430f01c6cfe3972d83fd7a347736
huskycat1202 <annie991202@naver.com>
2020-03-13 14:18:57 +0900

    20.03.13

---
+++ 1292.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+int a,b;
+int main(){
+    cin >> a >> b;
+    deque <int> dq;
+    int sum=0;
+    for(int i=1; i<=b; i++){
+        for(int j=0; j<i; j++){
+            dq.push_back(i);
+        }
+    }
+    for(int i=a-1; i<b; i++){
+        cout << dq.at(i) << "\n";
+        sum+=dq.at(i);
+    }
+    cout << sum;
+}
+/*
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+1 2 2 3 3 3 4 4 4 4  5  5  5  5  5
+*/
