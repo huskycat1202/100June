74448b72a6e9ba86444503805aaaffd8ec74c206
huskycat1202 <annie991202@naver.com>
2020-03-13 15:42:53 +0900

    20.03.13

---
+++ 1357.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+#include<string>
+using namespace std;
+string x,y;
+int main(){
+    cin >> x >> y;
+    int X=x.size(), Y=y.size(), A=0;
+    reverse(x.begin(),x.end()), reverse(y.begin(),y.end());
+    for(int i=X-1; i>=0; i--){
+        A+=pow(10,i)*(x[X-1-i]-'0');
+    }
+    for(int i=Y-1; i>=0; i--){
+        A+=pow(10,i)*(y[Y-1-i]-'0');
+    }
+    if(A==0) {cout << "0"; return 0;}
+    cout << A << " ";
+    bool x=1;
+    while(A!=0){
+        if(A%10==0 && x==1){
+            A/=10;
+        }
+        else{
+            cout << A%10;
+            A/=10;
+            x=0;
+        }
+    }
+}
