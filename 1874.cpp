7cdcd6c734b5f6a401d232e385d9cdcbf0e72c69
huskycat1202 <annie991202@naver.com>
2020-03-22 01:26:45 +0900

    20.03.21

--- 1874.cpp
+++ 1874.cpp
@@ -1,15 +1,23 @@
 #include<iostream>
 #include<stack>
+#include<vector>
 using namespace std;
 int main(){
-	int n;
+	int n, memo[100000]={};
+	stack <int> s;
+	vector <string> v;
 	cin >> n; // 8
-	int memo[100000]={};
 	for(int i=0; i<n; i++){
 		cin >> memo[i]; // 4 3 6 8 7 5 2 1
 	}
-	while(1){
-		
+	int i=1,k=0;
+	while(i!=n+1){
+		s.push(i); i++;
+		v.push_back("+\n");
+		while(!s.empty() && s.top()==memo[k]){s.pop(); v.push_back("-\n"); k++;}
+		if(!s.empty() && s.top()!=memo[k] && i==n+1){cout << "NO"; return 0;}
 	}
-	
+	for(int i=0; i<v.size(); i++){
+		cout << v[i];
+	}
 }
