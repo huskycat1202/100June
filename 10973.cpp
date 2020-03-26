e08e6c0bd4b48bbbac9d4a9a998f63466529a6bb
huskycat1202 <annie991202@naver.com>
2020-03-26 15:44:53 +0900

    20.03.26

---
+++ 10973.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n, x[10001]={}; cin >> n;
+    for(int i=1; i<=n; i++){
+        cin >> x[i];
+    }
+    if(!prev_permutation(x+1,x+n+1)) cout << -1;
+    else{
+        for(int i=1; i<=n; i++){
+            cout << x[i] << " ";
+        }
+    }
+}
