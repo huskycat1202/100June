966f9d2cdd541e74494d0084670995ad473f46c6
huskycat1202 <annie991202@naver.com>
2020-03-26 23:58:45 +0900

    20.03.26

--- 2503.cpp
+++ 2503.cpp
@@ -1,14 +1,35 @@
 #include<iostream>
+#include<string>
 using namespace std;
 int main(){
     int n; cin >> n;
-    int num1[101]={}, num2[101]={}, num3[101]={}, strike[101]={}, ball[101]={};
-    for(int i=0; i<n; i++){
-        cin >> num >> strike[i] >> ball[i];
-        num1[i]=num/100;
-        num2[i]=(num/10)%10;
-        num3[i]=num%10;
+    int possi[988]={};
+    for(int i=123; i<=987; i++){
+        string all=to_string(i);
+        if(all[0]==all[1]||all[1]==all[2]||all[2]==all[0]){possi[i]=1;} // 중복
+        if(all[0]-'0'==0 ||all[1]-'0'==0 ||all[2]-'0'==0){possi[i]=1;} // 0 불가 
     }
+    while(n--){
+        int q,strike,ball;
+        cin >> q >> strike >> ball;
+        string num = to_string(q);
+        for(int i=123; i<=987; i++){
+            int s=0,b=0;
+            string all=to_string(i);
+            for(int j=0; j<3; j++){
+                for(int k=0; k<3; k++){
+                    if(all[j]==num[k] && k==j) s++;
+                    else if(all[j]==num[k] && k!=j) b++;
+                }
+            }
+            if(strike!=s || ball!=b) possi[i]=1;
+        }
+    }
+    int cnt=0;
+    for(int i=123; i<=987; i++){
+        if(possi[i]==0) cnt++;
+    }
+    cout << cnt;
 }
 
 /*
