5c4234d151161102c5abdf8207163830c1ecbd1c
huskycat1202 <annie991202@naver.com>
2020-04-04 19:29:48 +0900

    20.04.04

--- 4949.cpp
+++ 4949.cpp
@@ -1,20 +1,31 @@
 #include<iostream>
 #include<string>
+#include<stack>
 using namespace std;
 
 int main(){
     while(1){
-        char x[100]; cin.getline(x,100,'\n');
-        if(x[0]=='.' && x[1]=='\0') return 0;
-        int a = 0, b=0;
+        stack <char> s;
+        int flag = 0;
+        string x; getline(cin,x);
+        if(x==".") return 0;
         for(int i=0; x[i]!='\0'; i++){
-            if(a<0 || b<0) break;
-            if(x[i]=='(') a++;
-            else if(x[i]==')') a--;
-            else if(x[i]=='[') b++;
-            else if(x[i]==']') b--;
+            if(x[i]=='(' || x[i]=='[') s.push(x[i]);
+            else if(x[i]==')'){
+                if(s.empty()) {cout << "no\n"; flag++; break;}
+                else if(s.top()=='(') s.pop();
+                else {cout << "no\n"; flag++; break;}
+            }
+            else if(x[i]==']'){
+                if(s.empty()) {cout << "no\n"; flag++; break;}
+                else if(s.top()=='[') s.pop();
+                else {cout << "no\n"; flag++; break;}
+            }
         }
-        if(a==0 && b==0) cout << "yes" << "\n";
-        else cout << "no" << "\n";  
+        if(flag==0){
+            if(s.empty()) cout << "yes\n";
+            else cout << "no\n";
+        }
+        
     }
 }
